bd56ca356891994abde78879bb1feb5f2242d5e0
Megan Hawkins <megan.hawkins28@gmail.com>
2023-01-27 16:55:46 -0500

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import TopNav from "./components/Navbars/TopNav";
+import WelcomePage from "./pages/WelcomePage"
+import DashboardPage from "./pages/DashboardPage";
+import FlashcardPage from "./pages/FlashcardPage";
+import ResourcePage from "./pages/ResourcePage";
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Lettttsss gooooo</h1>
-    </div>
+    <>
+      <TopNav />
+
+      <Routes>
+      <Route path='/welcome' element={<WelcomePage />} />
+        <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='/flashcards' element={<FlashcardPage />} />
+        <Route path='/resources' element={<ResourcePage />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Navbars/TopNav.jsx
+++ src/components/Navbars/TopNav.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { FaBars, FaRedhat } from 'react-icons/fa'
+import NavElements from "./NavElements"
 
 function TopNav() {
   return (
-    <div>TopNav</div>
+    <Nav>TopNav
+      <NavbarContainer >
+        <MobileIcon >
+          <FaBars />
+        </MobileIcon>   
+        <NavMenu>
+          <NavItem>
+            <NavLinks to="/dashboard">Home</NavLinks>
+          </NavItem>
+        </NavMenu>       
+        <NavMenu>
+          <NavItem>
+          <NavLinks to="/flashcards">Flashcards</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/resources">Resources</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/welcome">Welcome</NavLinks>
+          </NavItem>
+        </NavMenu>     
+      </NavbarContainer>
+
+
+    </Nav>
   )
 }
 
