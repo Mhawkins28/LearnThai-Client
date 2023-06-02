f92e9b0e6b9180b48fa1a579f5b7503d179c3e65
Megan Hawkins <megan.hawkins28@gmail.com>
2023-06-02 11:56:30 -0400

    data appearing correctly on both sides of card

--- src/components/Flashcards/FlashcardElements.jsx
+++ src/components/Flashcards/FlashcardElements.jsx
@@ -1,76 +1,49 @@
 import styled from "styled-components";
 
+export const CardContainer = styled.div`
+  .data {
+    margin: 10px auto;
+    background: #557c75;
+    height: 250px;
+    width: 450px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #000000;
+    font-size: 24px;
+    font-weight: 400;
+    border-radius: 0.25rem;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1), 0px 4px 8px rgba(0, 0, 0, 0.1),
+      0px 8px 16px rgba(0, 0, 0, 0.1), 0px 16px 32px rgba(0, 0, 0, 0.1);
+    position: relative;
+    cursor: pointer;
+    transition: 250ms;
+    transform-style: preserve-3d;
+    transform: perspective(1000px)
+      rotateY(${(props) => (props.side ? "-180deg" : "0deg")})
+      translateY(var(--translate-y, 0));
+  }
 
-export const CardContainer = styled.div `
-  
-.data {
-  margin: 10px auto;
-  background: #557c75;
-  height: 350px;
-  width: 350px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* flex-direction: column; */
-  color: #000000;
-  font-size: 24px;
-  font-weight: 400;
-  border-radius: var(--border-radius);
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1), 0px 4px 8px rgba(0, 0, 0, 0.1),
-    0px 8px 16px rgba(0, 0, 0, 0.1), 0px 16px 32px rgba(0, 0, 0, 0.1);
-  position: relative;
-  cursor: pointer;
-  /* animated interactivity */
-  transition: 250ms;
-  transform-style: preserve-3d;
-  transform: perspective(1000px) rotateY(var(--rotate-y, 0))
-    translateY(var(--translate-y, 0));
-}
+  .data .front,
+  .data .back {
+    position: absolute;
+    padding: 1rem;
+    backface-visibility: hidden;
+  }
 
-.side {
-  background: #557c75;
-  transform: rotateY(-180deg);
-}
+  .front {
+    transform: rotateY(0deg);
+    opacity: ${(props) => (props.side ? 0 : 1)};
+  }
 
-.data .front,
-.data .back {
-  position: absolute;
-  /* padding: 1rem; */
-  backface-visibility: hidden;
-}
+  .back {
+    transform: rotateY(180deg);
+    opacity: ${(props) => (props.side ? 1 : 0)};
+  }
+`;
 
-.data .front {
-  transform: rotateY(0deg);
-}
-
-.data.side .front {
-  opacity: 0;
-  display: none;
-}
-
-.data .back {
-  transform: rotateY(180deg);
-}
-`
-
-
-export const CardFront = styled.div`
-  
-
-`
-export const CardBack = styled.div`
-  
-
-`
-export const ButtonContainer =styled.section`
-  
-
-`
-export const PageContainer =styled.section`
-  
-
-`
-export const Button =styled.section`
-  
-
-`
+export const CardFront = styled.div``;
+export const CardBack = styled.div``;
+export const ButtonContainer = styled.section``;
+export const PageContainer = styled.section``;
+export const Button = styled.section``;
